d0858ca9b31a0fa17ca029b95bcf4969ef913c82
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-09-19 11:36:37 +0900

    cosmetic change

--- main.go
+++ main.go
@@ -7,27 +7,18 @@ import (
 )
 
 func main() {
-	var lines []string
+	lines := make(map[string]struct{})
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		text := scanner.Text()
-		found := false
-		for _, line := range lines {
-			if line == text {
-				found = true
-				break
-			}
-		}
-		if !found {
-			lines = append(lines, text)
+		if _, ok := lines[text]; !ok {
+			lines[text] = struct{}{}
+			fmt.Println(text)
 		}
 	}
 	if scanner.Err() != nil {
 		fmt.Fprintf(os.Stderr, "%v: %v\n", os.Args[0], scanner.Err())
 		os.Exit(1)
-	}
-	for _, line := range lines {
-		fmt.Println(line)
 	}
 }
