1488dd252e0cf2e62ceec6b29d572044d206f49e
Wei Shen <shenwei356@gmail.com>
2018-09-19 17:31:19 +0800

    buffered output

--- main.go
+++ main.go
@@ -7,18 +7,35 @@ import (
 )
 
 func main() {
+	if !isStdin() {
+		fmt.Fprintln(os.Stderr, "stdin not detected")
+		os.Exit(1)
+	}
+
 	lines := make(map[string]struct{})
 
 	scanner := bufio.NewScanner(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	var text string
+	var ok bool
 	for scanner.Scan() {
-		text := scanner.Text()
-		if _, ok := lines[text]; !ok {
+		text = scanner.Text()
+		if _, ok = lines[text]; !ok {
 			lines[text] = struct{}{}
-			fmt.Println(text)
+			out.WriteString(text + "\n")
 		}
 	}
 	if scanner.Err() != nil {
 		fmt.Fprintf(os.Stderr, "%v: %v\n", os.Args[0], scanner.Err())
 		os.Exit(1)
 	}
+	out.Flush()
+}
+
+func isStdin() bool {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+	return (stat.Mode() & os.ModeCharDevice) == 0
 }
